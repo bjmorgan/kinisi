8f5754fee4af877fde52a773239a0a22bebd48cb
Andrew McCluskey <andrew.mccluskey@diamond.ac.uk>
2020-04-17 12:24:19 +0100

    test coverage increase

--- kinisi/tests/test_arrhenius.py
+++ kinisi/tests/test_arrhenius.py
@@ -56,6 +56,45 @@ class TestArrhenius(unittest.TestCase):
         assert_equal(len(arr.variable_names), 3)
         assert_equal(len(arr.variable_units), 3)
 
+    def test_all_positive_priors(self):
+        """
+        Test the creation of an all positive prior.
+        """
+        temp = np.linspace(5, 50, 10)
+        ea = np.linspace(5, 50, 10)
+        dea = ea * 0.1
+        arr = arrhenius.StandardArrhenius(temp, ea, dea)
+        arr.max_likelihood()
+        priors = arr.all_positive_prior()
+        assert_equal(len(priors), 2)
+        assert_equal(priors[0].pdf(-1), 0)
+        assert_equal(priors[0].pdf(1000), 0)
+        assert_equal(priors[0].pdf(arr.variable_medians[0]), 1 / (arr.variable_medians[0] * 2 + arr.variable_medians[0]))
+        assert_equal(priors[1].pdf(-1), 0)
+        assert_equal(priors[1].pdf(1000), 0)
+        assert_equal(priors[1].pdf(arr.variable_medians[1]), 1 / (arr.variable_medians[1] * 2 + arr.variable_medians[1]))
+
+    def test_all_positive_priors_with_uu(self):
+        """
+        Test the creation of an all positive prior.
+        """
+        temp = np.linspace(5, 50, 10)
+        ea = np.linspace(5, 50, 10)
+        dea = ea * 0.1
+        arr = arrhenius.StandardArrhenius(temp, ea, dea, unaccounted_uncertainty=True)
+        arr.max_likelihood()
+        priors = arr.all_positive_prior()
+        assert_equal(len(priors), 3)
+        assert_equal(priors[0].pdf(-1), 0)
+        assert_equal(priors[0].pdf(1000), 0)
+        assert_equal(priors[0].pdf(arr.variable_medians[0]), 1 / (arr.variable_medians[0] * 2 + arr.variable_medians[0]))
+        assert_equal(priors[1].pdf(-1), 0)
+        assert_equal(priors[1].pdf(1000), 0)
+        assert_equal(priors[1].pdf(arr.variable_medians[1]), 1 / (arr.variable_medians[1] * 2 + arr.variable_medians[1]))
+        assert_equal(priors[2].pdf(-100), 0)
+        assert_equal(priors[2].pdf(1000), 0)
+        assert_equal(priors[2].pdf(arr.variable_medians[2]), 1 / (11))
+
     def test_super_arrhenius_init(self):
         """
         Test the initialisation of super arrhenius
