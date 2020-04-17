921eab4c98b1a44eae57ff96328d8c3d649e5b44
Andrew McCluskey <andrew.mccluskey@diamond.ac.uk>
2020-04-17 15:08:31 +0100

    add a test for a pymatgen file

--- kinisi/tests/test_parser.py
+++ kinisi/tests/test_parser.py
@@ -24,6 +24,9 @@ import unittest
 import numpy as np
 from numpy.testing import assert_almost_equal, assert_equal
 from pymatgen.core import Structure
+from pymatgen.io.vasp import Xdatcar
+import os
+import kinisi
 from kinisi import parser
 from uravu.distribution import Distribution
 from uravu.utils import straight_line
@@ -33,3 +36,29 @@ class TestParser(unittest.TestCase):
     """
     Unit tests for parser module
     """
+
+    def test_pymatgen_init(self):
+        xd = Xdatcar(os.path.join(os.path.dirname(kinisi.__file__), 'tests/inputs/example_XDATCAR.gz'))
+        da_params = { 'specie': 'Li',
+                      'time_step': 2.0,
+                      'step_skip': 50,
+                      'temperature': 400,
+                      'min_obs': 50}
+        data = parser.PymatgenParser(xd.structures, **da_params)
+        assert_almost_equal(data.time_step, 2.0)
+        assert_almost_equal(data.step_skip, 50)
+        assert_equal(data.indices, list(range(xd.natoms[0])))
+        assert_equal(1, 1)
+
+    def test_pymatgen_big_timestep(self):
+        xd = Xdatcar(os.path.join(os.path.dirname(kinisi.__file__), 'tests/inputs/example_XDATCAR.gz'))
+        da_params = { 'specie': 'Li',
+                      'time_step': 20.0,
+                      'step_skip': 100,
+                      'temperature': 400,
+                      'min_obs': 50}
+        data = parser.PymatgenParser(xd.structures, **da_params)
+        assert_almost_equal(data.time_step, 20.0)
+        assert_almost_equal(data.step_skip, 100)
+        assert_equal(data.indices, list(range(xd.natoms[0])))
+        assert_equal(1, 1)
