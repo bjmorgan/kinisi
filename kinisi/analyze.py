ce6ae96f1d07e237af0a4e398d6c74f92ad22b34
Andrew McCluskey <andrew.mccluskey@diamond.ac.uk>
2020-05-21 09:33:55 +0100

    refactor of the analyze code

--- kinisi/analyze.py
+++ kinisi/analyze.py
@@ -14,20 +14,16 @@ from kinisi import diffusion
 from kinisi.parser import MDAnalysisParser, PymatgenParser
 
 
-class DiffAnalyzer:
+
+class MSDAnalyzer:
     """
-    The :py:class:`kinisi.analyze.DiffAnalyzer` class performs analysis of diffusion relationships in materials. 
+    The :py:class:`kinisi.analyze.MSDAnalyzer` class evaluates the MSD of atoms in a material. 
     This is achieved through the application of a block bootstrapping methodology to obtain the most statistically accurate values for mean squared displacement and the associated uncertainty. 
-    The time-scale dependence of the MSD is then modeled with a straight line Einstein relationship, and Markov chain Monte Carlo is used to quantify inverse uncertainties for this model. 
 
     Attributes:
-        delta_t (:py:attr:`array_like`):  Timestep values. 
-        msd (:py:attr:`array_like`): The block bootstrap determined mean squared displacement values.
-        msd_err (:py:attr:`array_like`): A upper and lower uncertainty, at a 95 % confidence interval, of the mean squared displacement values.
+        dt (:py:attr:`array_like`):  Timestep values. 
         msd_distributions (:py:attr:`list` or :py:class:`Distribution`): The distributions describing the MSD at each timestep.
         relationship (:py:class:`kinisi.diffusion.Diffusion`): The :py:class:`~kinisi.diffusion.Diffusion` class object that describes the diffusion Einstein relationship.
-        D (:py:class:`uravu.distribution.Distribution`): The gradient of the Einstein relationship divided by 6 (twice the number of dimensions).
-        D_offset (:py:class:`uravu.distribution.Distribution`): The offset from the abscissa of the Einstein relationship.
 
     Args:
         trajectory (:py:attr:`str` or :py:attr:`list` of :py:attr:`str` or :py:attr:`list` of :py:class`pymatgen.core.structure.Structure`): The file path(s) that should be read by either the :py:class:`pymatgen.io.vasp.Xdatcar` or :py:class:`MDAnalysis.core.universe.Universe` classes, or a :py:attr:`list` of :py:class:`pymatgen.core.structure.Structure` objects ordered in sequence of run. 
@@ -55,27 +51,46 @@ class DiffAnalyzer:
 
         self.relationship = diffusion.Diffusion(self.dt, self.msd_distributions, bounds)
 
-        self.msd = self.relationship.y.n
-        self.msd_err = self.relationship.y.s[0]
+    @property
+    def msd(self):
+        """
+        Returns MSD for the input trajectories.
 
-        self.relationship.max_likelihood('diff_evo')
-        self.relationship.mcmc()
+        Returns:
+            :py:attr:`array_like`: MSD values.
+        """
+        return self.relationship.y.n
 
-        self.D = self.relationship.diffusion_coefficient
-        self.D_offset = self.relationship.variables[1]
+    @property
+    def msd_err(self):
+        """
+        Returns MSD uncertainty for the input trajectories.
+
+        Returns:
+            :py:attr:`array_like`: A lower and upper uncertainty, at a 95 % confidence interval, of the mean squared displacement values..
+        """
+        return self.relationship.y.s
+
+    @property
+    def ci(self):
+        """
+        Returns MSD confidence inteval, at 95 %, for the input trajectories.
+
+        Returns:
+            :py:attr:`array_like`: A lower and upper 95 % confidence interval of the mean squared displacement values..
+        """ 
+        return np.array([self.msd - self.msd_err[0], self.msd + self.msd_err[1]])
 
 
-class MSDAnalyzer:
+class DiffAnalyzer(MSDAnalyzer):
     """
-    The :py:class:`kinisi.analyze.MSDAnalyzer` class evaluates the MSD of atoms in a material. 
+    The :py:class:`kinisi.analyze.DiffAnalyzer` class performs analysis of diffusion relationships in materials. 
     This is achieved through the application of a block bootstrapping methodology to obtain the most statistically accurate values for mean squared displacement and the associated uncertainty. 
+    The time-scale dependence of the MSD is then modeled with a straight line Einstein relationship, and Markov chain Monte Carlo is used to quantify inverse uncertainties for this model. 
 
     Attributes:
-        dt (:py:attr:`array_like`):  Timestep values. 
-        msd (:py:attr:`array_like`): The block bootstrap determined mean squared displacement values.
-        msd_err (:py:attr:`array_like`): A upper and lower uncertainty, at a 95 % confidence interval, of the mean squared displacement values.
-        msd_distributions (:py:attr:`list` or :py:class:`Distribution`): The distributions describing the MSD at each timestep.
-        relationship (:py:class:`kinisi.diffusion.Diffusion`): The :py:class:`~kinisi.diffusion.Diffusion` class object that describes the diffusion Einstein relationship.
+        D (:py:class:`uravu.distribution.Distribution`): The gradient of the Einstein relationship divided by 6 (twice the number of dimensions).
+        D_offset (:py:class:`uravu.distribution.Distribution`): The offset from the abscissa of the Einstein relationship.
 
     Args:
         trajectory (:py:attr:`str` or :py:attr:`list` of :py:attr:`str` or :py:attr:`list` of :py:class`pymatgen.core.structure.Structure`): The file path(s) that should be read by either the :py:class:`pymatgen.io.vasp.Xdatcar` or :py:class:`MDAnalysis.core.universe.Universe` classes, or a :py:attr:`list` of :py:class:`pymatgen.core.structure.Structure` objects ordered in sequence of run. 
@@ -83,26 +98,18 @@ class MSDAnalyzer:
         dtype (:py:attr:`str`, optional): The file format, for the :py:class:`kinisi.parser.PymatgenParser` this should be :py:attr:`'Xdatcar'` and for :py:class:`kinisi.parser.MDAnalysisParser` this should be the appropriate format to be passed to the :py:class:`MDAnalysis.core.universe.Universe`. Defaults to :py:attr:`'Xdatcar'`.
         bounds (:py:attr:`tuple`, optional): Minimum and maximum values for the gradient and intercept of the diffusion relationship. Defaults to :py:attr:`((0, 100), (-10, 10))`. 
     """
-    def __init__(self, trajectory, params, dtype='Xdatcar'):  # pragma: no cover
-        if dtype is 'Xdatcar':
-            xd = Xdatcar(trajectory)
-            u = PymatgenParser(xd.structures, **params)
-        elif dtype is 'structures':
-            u = PymatgenParser(trajectory, **params)
-        else:
-            universe = mda.Universe(*trajectory, format=dtype)
-            u = MDAnalysisParser(universe, **params)
+    def __init__(self, trajectory, params, dtype='Xdatcar', bounds=((0, 100), (-10, 10))):  # pragma: no cover
+        super.__init__(trajectory, params, dtype, bounds)
 
-        dt = u.delta_t
-        disp_3d = u.disp_3d
+        self.relationship.max_likelihood('diff_evo')
+        self.relationship.mcmc()
 
-        diff_data = diffusion.msd_bootstrap(dt, disp_3d)
+    @property
+    def D(self):
+        return self.relationship.diffusion_coefficient
 
-        self.dt = diff_data[0]
-        self.msd_distributions = diff_data[3]
+    @property
+    def D_offset(self):
+        return self.relationship.variables[1]
 
-        self.relationship = diffusion.Diffusion(self.dt, self.msd_distributions, ((0, 100), (-10, 10)))
-
-        self.msd = self.relationship.y.n
-        self.msd_err = self.relationship.y.s[0]
-
+    
