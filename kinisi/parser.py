866353a23262415ce169259f085858702a7447c1
Andrew McCluskey <andrew.mccluskey@ess.eu>
2021-07-18 12:05:10 +0200

    Update parser.py

--- kinisi/parser.py
+++ kinisi/parser.py
@@ -107,7 +107,7 @@ class PymatgenParser(Parser):
     A parser for pymatgen structures.
 
     Args:
-        structures (:py:attr:`list` or :py:class`pymatgen.core.structure.Structure`): Structures ordered in sequence of run.
+        structures (:py:attr:`list` or :py:class:`pymatgen.core.structure.Structure`): Structures ordered in sequence of run.
         specie (:py:class:`pymatgen.core.periodic_table.Element` or :py:class:`pymatgen.core.periodic_table.Specie`): Specie to calculate diffusivity for as a String, e.g. :py:attr:`'Li'`.
         time_step (:py:attr:`float`): Time step, in simulation units, between steps in trajectory.
         step_step (:py:attr:`int`): Sampling freqency of the trajectory (time_step is multiplied by this number to get the real time between output from the simulation file).
