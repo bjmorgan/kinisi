a9440a967e389a02e3c54ee51b70c090b6cbe346
Andrew McCluskey <andrew.mccluskey@ess.eu>
2021-11-11 10:23:43 +0100

    trying again to fix the bullet list

--- kinisi/analyze.py
+++ kinisi/analyze.py
@@ -2,10 +2,9 @@
 This module contains the API classes for :py:mod:`kinisi`.
 It is anticipated that this is where the majority of interaction with the package will occur.
 This module includes:
-
-- the :py:class:`kinisi.analyze.DiffusionAnalyzer` class for MSD and diffusion analysis;
-- the :py:class:`kinisi.analyze.JumpDiffusionAnalyzer` class for TMSD and collective diffusion analysis; and
-- the :py:class:`kinisi.analyze.ConductivityAnalyzer` class for MSCD and conductivity analysis.
+* the :py:class:`kinisi.analyze.DiffusionAnalyzer` class for MSD and diffusion analysis;
+* the :py:class:`kinisi.analyze.JumpDiffusionAnalyzer` class for TMSD and collective diffusion analysis; and
+* the :py:class:`kinisi.analyze.ConductivityAnalyzer` class for MSCD and conductivity analysis.
 
 These are all compatible with VASP Xdatcar output files, pymatgen structures and any MD trajectory that the
 :py:mod:`MDAnalysis` package can handle.
