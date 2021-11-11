ea04ba566bb3de4d3043ce61ec3ccc58ce1e6b40
Andrew McCluskey <andrew.mccluskey@ess.eu>
2021-11-11 10:51:25 +0100

    add rtd to extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,7 @@ release = __version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme'
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.todo',
