9e81ccf9a7a2a346a0b66586b93c2122dbd88cd3
arm61 <andrew@mccluskey.scot>
2022-11-23 07:14:35 +0100

    Remove support for 3.7

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ INFO = {
                         'License :: OSI Approved :: MIT License',
                         'Natural Language :: English',
                         'Operating System :: OS Independent',
-                        'Programming Language :: Python :: 3.7',
                         'Programming Language :: Python :: 3.8',
                         'Programming Language :: Python :: 3.9',
                         'Programming Language :: Python :: 3.10',
