058ca39671e375e380a5399d07f1271b9b306de0
Andrew R. McCluskey <andrew.mccluskey@bristol.ac.uk>
2025-07-08 16:48:22 +0100

    Add the Samples class

---
+++ kinisi/samples.py
@@ -0,0 +1,42 @@
+"""
+A class to represent samples of a physical quantity using scipp.
+This class extends the scipp.Variable class to provide additional functionality
+for handling samples, such as calculating the mean and standard deviation of the samples.
+"""
+
+# Copyright (c) kinisi developers.
+# Distributed under the terms of the MIT License.
+# author: Andrew R. McCluskey (arm61)
+
+import scipp as sc
+from uncertainties import ufloat
+
+class Samples(sc.Variable):
+    """
+    A subclass of scipp.Variable that represents samples of a physical quantity.
+    This class is designed to add some specific functionality for handling
+    samples, such as calculating the mean and standard deviation of the samples.
+    It also overrides the HTML representation to include these statistics.
+    
+    :param values: The values of the samples.
+    :param unit: The unit of the samples, if applicable. Optional, defaults to dimensionless.
+    """
+    def __init__(self, values, unit=sc.units.dimensionless):
+        super().__init__(values=values, unit=unit, dims=['samples'])
+    
+    def _repr_html_(self) -> str:
+        """
+        This function augments the default HTML representation of a scipp Variable
+        to include the mean and standard deviation of the samples.
+        """
+        split_1 = sc.make_html(self).split('sc-value-preview sc-preview')
+        split_2 = split_1[1].split('div')
+        split_2[1] = '>' + ufloat(sc.mean(self).value, sc.std(self, ddof=1).value).__str__() + '</'
+        split_1[1] = 'div'.join(split_2)
+        split_3 = 'sc-value-preview sc-preview'.join(split_1).split('sc-obj-type')
+        split_4 = split_3[3].split('div')
+        split_5 = split_4[0].split()
+        split_5[0] = '\'>kinisi.Samples'
+        split_4[0] = ' '.join(split_5)
+        split_3[3] = 'div'.join(split_4)
+        return 'sc-obj-type'.join(split_3)
---
+++ kinisi/tests/test_samples.py
@@ -0,0 +1,44 @@
+"""
+Tests for the samples module
+"""
+
+# Copyright (c) kinisi developers.
+# Distributed under the terms of the MIT License.
+# author: Andrew R. McCluskey (arm61)
+
+import unittest
+import numpy as np
+import scipp as sc
+from kinisi.samples import Samples
+
+class TestSamples(unittest.TestCase):
+    """
+    Unit tests for the Samples class.
+    """
+
+    def test_samples_initialization(self):
+        """
+        Test the initialization of the Samples class.
+        """
+        values = np.array([1.0, 2.0, 3.0])
+        samples = Samples(values)
+        self.assertEqual(samples.dims, ('samples',))
+        np.testing.assert_array_equal(samples.values, values)
+        self.assertEqual(samples.unit, 'dimensionless')
+
+    def test_samples_unit(self):
+        """
+        Test the unit of the Samples class.
+        """
+        values = np.array([1.0, 2.0, 3.0])
+        samples = Samples(values, sc.Unit('m'))
+        self.assertEqual(samples.unit, sc.units.m)
+
+    def test_samples_repr_html(self):
+        """
+        Test the HTML representation of the Samples class.
+        """
+        values = np.array([1.0, 2.0, 3.0])
+        samples = Samples(values)
+        html_repr = samples._repr_html_()
+        self.assertIn('kinisi.Samples', html_repr)
