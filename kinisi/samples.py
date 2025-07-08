e3bf2f77127f8dd7290af785a63a90ef000c16e6
Andrew R. McCluskey <andrew.mccluskey@bristol.ac.uk>
2025-07-08 17:05:20 +0100

    Ruff formatting

--- kinisi/samples.py
+++ kinisi/samples.py
@@ -18,13 +18,14 @@ class Samples(sc.Variable):
     This class is designed to add some specific functionality for handling
     samples, such as calculating the mean and standard deviation of the samples.
     It also overrides the HTML representation to include these statistics.
-    
+
     :param values: The values of the samples.
     :param unit: The unit of the samples, if applicable. Optional, defaults to dimensionless.
     """
+
     def __init__(self, values, unit=sc.units.dimensionless):
         super().__init__(values=values, unit=unit, dims=['samples'])
-    
+
     def _repr_html_(self) -> str:
         """
         This function augments the default HTML representation of a scipp Variable
@@ -37,7 +38,7 @@ class Samples(sc.Variable):
         split_3 = 'sc-value-preview sc-preview'.join(split_1).split('sc-obj-type')
         split_4 = split_3[3].split('div')
         split_5 = split_4[0].split()
-        split_5[0] = '\'>kinisi.Samples'
+        split_5[0] = "'>kinisi.Samples"
         split_4[0] = ' '.join(split_5)
         split_3[3] = 'div'.join(split_4)
         return 'sc-obj-type'.join(split_3)
