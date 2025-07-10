fc749f5f054bbc1892469c39c9b4d60229ddda2c
Andrew R. McCluskey <andrew.mccluskey@bristol.ac.uk>
2025-07-10 12:07:02 +0100

    nested sampling and tests

--- kinisi/arrhenius.py
+++ kinisi/arrhenius.py
@@ -15,6 +15,7 @@ from collections.abc import Callable
 
 import numpy as np
 import scipp as sc
+from dynesty import NestedSampler
 from emcee import EnsembleSampler
 from scipp.constants import N_A, R
 from scipp.typing import VariableLike
@@ -34,44 +35,77 @@ class TemperatureDependent:
 
     :param diffusion: Diffusion coefficient sc.DataFrame with a temperature coordinate and variances.
     :param function: A callable function that describes the relationship between temperature and diffusion.
+    :param parameter_names: A tuple of parameter names for the function.
+    :param parameter_units: A tuple of sc.Unit objects corresponding to the parameter names.
     :param bounds: Optional bounds for the parameters of the function. Defaults to None, in which case these
         are defined as +/- 50 percent of the best fit values.
     """
 
-    def __init__(self, diffusion, function: Callable, bounds: None = None) -> 'TemperatureDependent':
+    def __init__(
+        self,
+        diffusion,
+        function: Callable,
+        parameter_names: tuple[str],
+        parameter_units: tuple[sc.Unit],
+        bounds: None = None,
+    ) -> 'TemperatureDependent':
         self.diffusion = diffusion
         self.temperature = diffusion.coords['temperature']
         self.data_group = sc.DataGroup({'data': diffusion})
         self.function = function
         self.bounds = bounds
+        self.parameter_names = parameter_names
+        self.parameter_units = parameter_units
+        print(bounds)
+        print(self.parameter_names)
+        if bounds is not None and len(bounds) != len(self.parameter_names):
+            raise ValueError(
+                f'Bounds must be a tuple of length {len(self.parameter_names)}, got {len(bounds)} instead.'
+            )
         self.max_likelihood()
         if self.bounds is None:
-            self.bounds = tuple([(p.value * 0.5 * p.unit, p.value * 1.5 * p.unit) for p in self._parameters])
+            self.bounds = tuple(
+                [
+                    (
+                        self.data_group[p].value * 0.5 * self.data_group[p].unit,
+                        self.data_group[p].value * 1.5 * self.data_group[p].unit,
+                    )
+                    for p in self.parameter_names
+                ]
+            )
         self.priors = [uniform(b[0].value, b[1].value - b[0].value) for b in self.bounds]
 
     def __repr__(self):
         """
-        Return a string representation of the TemperatureDependent object.
+        :return: a string representation of the TemperatureDependent object.
         """
         return self.data_group.__repr__()
 
     def __str__(self):
         """
-        Return a string representation of the TemperatureDependent object.
+        :return: a string representation of the TemperatureDependent object.
         """
         return self.data_group.__str__()
 
     def _repr_html_(self):
         """
-        Return an HTML representation of the TemperatureDependent object.
+        :return: an HTML representation of the TemperatureDependent object.
         """
         return self.data_group._repr_html_()
+
+    @property
+    def logz(self) -> sc.Variable:
+        """
+        :return: The log evidence of the model.
+        """
+        return self.data_group['logz']
 
     def log_likelihood(self, parameters: tuple[float]) -> float:
         """
         Calculate the likelihood of the model given the data.
 
         :param model: The model to evaluate.
+
         :return: The likelihood of the model.
         """
         model = self.function(self.temperature.values, *parameters)
@@ -117,6 +151,19 @@ class TemperatureDependent:
         """
         return self.log_likelihood(parameters) + self.log_prior(parameters)
 
+    def prior_transform(self, parameters: tuple[float]) -> tuple[float]:
+        """
+        Transform the parameters from the prior space to the parameter space.
+
+        :param parameters: The parameters of the model in prior space.
+
+        :return: The parameters of the model in parameter space.
+        """
+        x = np.array(parameters)
+        for i in range(len(x)):
+            x[i] = self.priors[i].ppf(parameters[i])
+        return x
+
     def max_likelihood(self) -> tuple[float]:
         """
         Find the best fit parameters for the model.
@@ -126,7 +173,8 @@ class TemperatureDependent:
             x0 = [((b[1] + b[0]) / 2) for b in self.bounds]
         else:
             x0 = [1 * u for u in self.parameter_units]
-        result = minimize(self.nll, [x.value for x in x0]).x
+        bounds = [(b[0].value, b[1].value) for b in self.bounds] if self.bounds is not None else None
+        result = minimize(self.nll, [x.value for x in x0], bounds=bounds).x
         for i, name in enumerate(self.parameter_names):
             self.data_group[name] = result[i] * self.parameter_units[i]
 
@@ -135,11 +183,11 @@ class TemperatureDependent:
         Perform MCMC sampling of the model parameters.
         This will sample the activation energy and preexponential factor.
         """
-        if isinstance(self._parameters[0], Samples):
-            values = np.array([sc.mean(p).value for p in self._parameters])
+        if isinstance(self.data_group[self.parameter_names[0]], Samples):
+            values = np.array([sc.mean(self.data_group[p]).value for p in self.parameter_names])
         else:
-            values = np.array([p.value for p in self._parameters])
-        pos = values + values * 1e-2 * np.random.randn(n_walkers, len(self._parameters))
+            values = np.array([self.data_group[p].value for p in self.parameter_names])
+        pos = values + values * 1e-2 * np.random.randn(n_walkers, len(self.parameter_names))
         nwalkers, ndim = pos.shape
 
         sampler = EnsembleSampler(nwalkers, ndim, self.log_posterior)
@@ -147,6 +195,19 @@ class TemperatureDependent:
         flatchain = sampler.get_chain(discard=n_burn, thin=n_thin, flat=True)
         for i, name in enumerate(self.parameter_names):
             self.data_group[name] = Samples(flatchain[:, i], unit=self.parameter_units[i])
+
+    def nested_sampling(self) -> None:
+        """
+        Perform nested sampling to estimate the Bayesian evidence of the model.
+        """
+        sampler = NestedSampler(self.log_likelihood, self.prior_transform, ndim=len(self.parameter_names))
+        sampler.run_nested()
+        self.data_group['logz'] = sc.scalar(
+            value=sampler.results.logz[-1], variance=sampler.results.logzerr[-1], unit=sc.units.dimensionless
+        )
+        equal_weighted_samples = sampler.results.samples_equal()
+        for i, name in enumerate(self.parameter_names):
+            self.data_group[name] = Samples(equal_weighted_samples[:, i], unit=self.parameter_units[i])
 
     @property
     def flatchain(self) -> sc.DataGroup:
@@ -185,16 +246,10 @@ class Arrhenius(TemperatureDependent):
         diffusion,
         bounds: tuple[tuple[VariableLike, VariableLike], tuple[VariableLike, VariableLike]] | None = None,
     ) -> 'Arrhenius':
-        self.parameter_names = ('activation_energy', 'preexponential_factor')
-        self.parameter_units = (sc.Unit('eV'), sc.Unit('cm^2/s'))
-        super().__init__(diffusion, function=arrhenius, bounds=bounds)
+        parameter_names = ('activation_energy', 'preexponential_factor')
+        parameter_units = (sc.Unit('eV'), sc.Unit('cm^2/s'))
 
-    @property
-    def _parameters(self) -> tuple[VariableLike, VariableLike]:
-        """
-        :return: The parameters of the Arrhenius model.
-        """
-        return (self.activation_energy, self.preexponential_factor)
+        super().__init__(diffusion, arrhenius, parameter_names, parameter_units, bounds=bounds)
 
     @property
     def activation_energy(self) -> VariableLike | Samples:
@@ -209,3 +264,63 @@ class Arrhenius(TemperatureDependent):
         :return: Preexponential factor.
         """
         return self.data_group['preexponential_factor']
+
+
+def vtf_equation(
+    abscissa: VariableLike, activation_energy: VariableLike, preexponential_factor: VariableLike, T0: VariableLike
+) -> VariableLike:
+    """
+    Evaluate the Vogel-Fulcher-Tammann equation.
+
+    :param abscissa: The temperature data.
+    :param activation_energy: The apparent activation energy value.
+    :param preexponential_factor: The preexponential factor value.
+    :param T0: The T0 value.
+
+    :return: The diffusion coefficient data.
+    """
+    return preexponential_factor * np.exp(-activation_energy / (R_eV.values * (abscissa - T0)))
+
+
+class VogelFulcherTammann(TemperatureDependent):
+    """
+    Evaluate the data with a Vogel-Fulcher-Tammann relationship.
+
+    :param diffusion: Diffusion coefficient sc.DataFrame with a temperature coordinate and variances.
+    :param bounds: Optional bounds for the parameters of the function. Defaults to None, in which case these
+        are defined as +/- 50 percent of the best fit values.
+    """
+
+    def __init__(
+        self,
+        diffusion,
+        bounds: tuple[
+            tuple[VariableLike, VariableLike], tuple[VariableLike, VariableLike], tuple[VariableLike, VariableLike]
+        ]
+        | None = None,
+    ) -> 'VogelFulcherTammann':
+        parameter_names = ('activation_energy', 'preexponential_factor', 'T0')
+        parameter_units = (sc.Unit('eV'), sc.Unit('cm^2/s'), sc.Unit('K'))
+
+        super().__init__(diffusion, vtf_equation, parameter_names, parameter_units, bounds=bounds)
+
+    @property
+    def activation_energy(self) -> VariableLike | Samples:
+        """
+        :return: Activated energy distribution in electronvolt.
+        """
+        return self.data_group['activation_energy']
+
+    @property
+    def preexponential_factor(self) -> VariableLike | Samples:
+        """
+        :return: Preexponential factor.
+        """
+        return self.data_group['preexponential_factor']
+
+    @property
+    def T0(self) -> VariableLike | Samples:
+        """
+        :return: Temperature factor for the VTF equation in kelvin.
+        """
+        return self.data_group['T0']
